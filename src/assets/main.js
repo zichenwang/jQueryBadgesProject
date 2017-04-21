589f75a414b901f068a1f11782a0ef5e422823ba
zichenwang <zwang@smsassist.com>
2017-04-21 10:09:39 -0500

    all tasks completed

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,24 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/ZichenWang.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      response.courses.completed.forEach(function(course){
+        $div = $('<div />').addClass('course').appendTo('#badges');
 
+        $('<h3 />').text(course.title).appendTo($div);
+
+        $('<img />').attr('src', course.badge).appendTo($div);
+
+        $('<a />', {
+          'class': 'btn btn-primary',
+          target: '_blank',//open in a new tab
+          href: course.url,
+          text: 'See Course'
+        }).appendTo($div);
+      });
+    }
+  })
 });
